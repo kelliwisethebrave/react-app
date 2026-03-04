1e6b91dfed8ec41264cea7617a99d4d642ffbd81
kelliwisethebrave <198285550+kelliwisethebrave@users.noreply.github.com>
2026-03-03 17:03:04 -0700

    Add changes up to 29 min 44 sec

---
+++ src/Message.tsx
@@ -0,0 +1,9 @@
+//PascalCasing - capitalize first letter in word
+function Message() {
+  const name = "";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello World</h1>;
+  // JSX: JavaScript XML
+}
+
+export default Message;
